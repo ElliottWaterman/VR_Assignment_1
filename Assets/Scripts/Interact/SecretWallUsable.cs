474d51717f3475a786b2b5e81f811383fe6f9c13
ElliottWaterman <elliott@aquaman.plus.com>
2019-02-06 00:43:10 +0000

    Add: Secret usable wall, stained glass material; Remove: Old models, Nature Kit assets

---
+++ Assets/Scripts/Interact/SecretWallUsable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretWallUsable : UsableObject
+{
+    public Renderer wallObject;
+
+    public Material stainedGlass;
+    public Material creamWall;
+
+    private bool secretUsed = false;
+
+    public override void OnUse()
+    {
+        // Toggle secret found
+        secretUsed = !secretUsed;
+
+        // Secret used then set to stained glass, else set to cream wall
+        wallObject.material = (secretUsed ? stainedGlass : creamWall);
+    }
+
+    public override void DisplayText()
+    {
+        // Display hint! Shh it's a secret!
+        this.interactText.enabled = true;
+        this.interactText.text = "...";
+    }
+}
