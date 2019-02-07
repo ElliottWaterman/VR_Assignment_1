a7d6d2fe04101a9502df58f07ff8aa762e5e55e4
ElliottWaterman <elliott@aquaman.plus.com>
2019-02-07 01:26:53 +0000

    Add: Dark fantasy kit models, rigidbody ball; Update: Hobbiton scene with models and lighting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    private const float RUNNING_SPEED = 15.0f;
+    private const float WALKING_SPEED = 10.0f;
+
+    public float speed = WALKING_SPEED;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Lock mouse into game screen
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Holding shift key makes player run
+        speed = (Input.GetKey(KeyCode.LeftShift) ? RUNNING_SPEED : WALKING_SPEED);
+
         // Gets vertical movement from input keys (left, right, a, d)
         float translation = Input.GetAxis("Vertical") * speed;
         // Gets horizontal movement from input keys (up, down, w, s)
@@ -26,16 +33,16 @@ public class PlayerController : MonoBehaviour
         transform.Translate(straffe, 0, translation);
 
         // Get back cursor if escape key is pressed
-        if (Input.GetKeyDown("escape"))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (Cursor.lockState == CursorLockMode.Locked)
             {
-                Debug.Log("Unlocked");
+                Debug.Log("Cursor Unlocked");
                 Cursor.lockState = CursorLockMode.None;
             }
             else if (Cursor.lockState == CursorLockMode.None)
             {
-                Debug.Log("Locked");
+                Debug.Log("Cursor Locked - Click mouse in screen");
                 Cursor.lockState = CursorLockMode.Locked;
             }
         }
